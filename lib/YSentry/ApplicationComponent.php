67c95f67fd780de1e197da1bc7acfe7b6c5fc8f7
Shiki <shikishiji@gmail.com>
2013-09-03 09:19:48 +0800

    Handling for errors, exceptions, and fatal (unrecoverable) errors. This functionality requires the component to be preloaded by Yii. This can be set in the Yii config. Handling can be turned off if desired.

--- lib/YSentry/ApplicationComponent.php
+++ lib/YSentry/ApplicationComponent.php
@@ -94,10 +94,21 @@ class ApplicationComponent extends \CApplicationComponent
   public $clients;
 
   /**
+   * Whether to automatically log errors, exceptions, and fatal errors. If enabled, this component
+   * will register for `onException`, `onError`, and `onEndRequest` events of the current Yii
+   * {@link CApplication} to log any received exceptions. Defaults to `true`.
+   *
+   * @var boolean
+   */
+  public $enableExceptionHandling = true;
+
+  /**
    *
    * @var array
    */
   protected $_clientInstances = array();
+
+  protected $_reservedMemory;
 
   /**
    * {@inheritdoc}
@@ -107,7 +118,89 @@ class ApplicationComponent extends \CApplicationComponent
     if (!is_array($this->clients))
       $this->clients = array();
 
+    if ($this->enableExceptionHandling)
+      $this->registerEventHandlers();
+
     parent::init();
+  }
+
+  /**
+   * Register event handlers to log errors, exceptions, and fatal errors.
+   */
+  protected function registerEventHandlers()
+  {
+    $app = \Yii::app();
+    $app->attachEventHandler('onException', array($this, 'onExceptionEvent'));
+    $app->attachEventHandler('onError', array($this, 'onErrorEvent'));
+
+    // Fatal errors are handled differently because PHP because PHP does not call the method
+    // specified by {@link set_error_handler} if PHP failed because of a FATAL (unrecoverable)
+    // error. We handle fatal errors by attaching to Yii's shutdown event (`onEndRequest`) and
+    // get the error from there.
+    $this->reserveMemoryForShutdownHandling();
+    $app->attachEventHandler('onEndRequest', array($this, 'onEndRequestEvent'));
+  }
+
+  /**
+   * Common method used by the event handlers to send an exception to Sentry. You can override this
+   * method for fine-grained control on what type of exceptions you want to be sent to Sentry.
+   *
+   * @param Exception $e
+   */
+  protected function captureException(\Exception $e)
+  {
+    $this->getClient()->captureException($e);
+  }
+
+  /**
+   * Event handler for {@link CApplication}'s `onException` event.
+   *
+   * @param CExceptionEvent $event
+   */
+  public function onExceptionEvent(\CExceptionEvent $event)
+  {
+    $this->captureException($event->exception);
+  }
+
+  /**
+   * Event handler for {@link CApplication}'s `onError` event.
+   *
+   * @param CErrorEvent $event
+   */
+  public function onErrorEvent(\CErrorEvent $event)
+  {
+    $e = new \ErrorException($event->message, $event->code, $event->code, $event->file, $event->line);
+
+    $this->captureException($e);
+  }
+
+  /**
+   * Event handler for {@link CApplication}'s `onEndRequest` event. This event handler is called
+   * when the Yii application ends normally or the app will shut down because of a fatal error.
+   *
+   * The error handling here is based on {@link Raven_ErrorHandler}'s fatal error handler. We
+   * couldn't use that class directly because we are depending on Yii's own shutdown handler.
+   *
+   * @param CEvent $event
+   */
+  public function onEndRequestEvent(\CEvent $event)
+  {
+    if (!($lastError = error_get_last()))
+      return;
+
+    unset($this->_reservedMemory);
+
+    // We will only handle these error types because other types would already have been sent
+    // through
+    $errorTypes = array(E_ERROR, E_PARSE, E_CORE_ERROR, E_CORE_WARNING, E_COMPILE_ERROR,
+      E_COMPILE_WARNING, E_STRICT);
+    if (!in_array($lastError['type'], $errorTypes))
+      return;
+
+    $e = new \ErrorException(@$lastError['message'], @$lastError['type'], @$lastError['type'],
+      @$lastError['file'], @$lastError['line']);
+
+    $this->captureException($e);
   }
 
   /**
@@ -144,6 +237,16 @@ class ApplicationComponent extends \CApplicationComponent
     $options = isset($config['options']) ? $config['options'] : array();
 
     return new \Raven_Client($dsn, $options);
+  }
+
+  /**
+   * Taken from {@link Raven_ErrorHandler}, reserves some memory for shutdown handling.
+   *
+   * @param integer $size
+   */
+  protected function reserveMemoryForShutdownHandling($size = 10)
+  {
+    $this->_reservedMemory = str_repeat('x', 1024 * $size);
   }
 }
 
